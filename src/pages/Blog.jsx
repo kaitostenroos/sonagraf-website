9ba340ca7ab1b02f61da733ea62b12dc23b99e6d
Kaito Stenroos <kaito.stenroos@outlook.com>
2025-04-27 18:36:56 +0300

    Improve blog page appearance

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -7,22 +7,29 @@ function Blog() {
     return(
         <div className="blog-wrapper">
             <h1>News</h1>
-            <Link to={"test-post"} className="blog-entry">
-                <img src={exampleImage} />
-                <p>Example Post</p>
-            </Link>
-            <Link to={"test-post"} className="blog-entry">
-                <img src={exampleImage} />
-                <p>Example Post</p>
-            </Link>
-            <Link to={"test-post"} className="blog-entry">
-                <img src={exampleImage} />
-                <p>Example Post</p>
-            </Link>
-            <Link to={"test-post"} className="blog-entry">
-                <img src={exampleImage} />
-                <p>Example Post</p>
-            </Link>
+            <div className="blog-entry-wrapper">
+                <Link to={"test-post"} className="blog-entry">
+                    <img src={exampleImage} />
+                    <p className="date">1/12/2025</p>
+                    <p>Example Post</p>
+                </Link>
+                <Link to={"test-post"} className="blog-entry">
+                    <img src={exampleImage} />
+                    <p className="date">1/12/2025</p>
+                    <p>Example Post</p>
+                </Link>
+                <Link to={"test-post"} className="blog-entry">
+                    <img src={exampleImage} />
+                    <p className="date">1/12/2025</p>
+                    <p>Example Post</p>
+                </Link>
+                <Link to={"test-post"} className="blog-entry">
+                    <img src={exampleImage} />
+                    <p className="date">1/12/2025</p>
+                    <p>Example Post</p>
+                </Link>
+            </div>
+            
         </div>
     )
 }
