92957650a895dac7855147676fa8e3e446693783
Kaito Stenroos <kaito.stenroos@outlook.com>
2025-05-02 19:10:21 +0300

    Add content to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,6 +7,39 @@ function About() {
     return(
         <div className="about-wrapper">
             <h1>About</h1>
+            <article className='about-text-container'>
+                <h2>Background</h2>
+                <p>
+                    SonaGraf started as kabusiki-kaisha (Ltd.)(株式会社SonaGraf) in Japan at 2022, founded by award-winning CG artist Jaakko Saari. 
+                    <br />
+                    <br />
+                    SonaGraf has worked with several high end brands such as Canon and Nikon, and provided training and services for large companies in Japan and world-wide.
+                    <br />
+                    <br />
+                    We have since then transitioned away into a trade name SonaGraf, in order to be able to provide quality service that is even more cost effective, with extreme focus on quality.
+                </p>
+                <h2>Focus</h2>
+                    <p>
+                    SonaGraf focuses on non-game real-time CG production and training using Unreal as our main tool. Although we sometimes work on game related projects the work we do is mostly related to cinematics.
+                    <br />
+                    <br />
+                    Our strength is providing quality at cost-effective price.
+                    </p>
+                <h2>Values</h2>
+                <p>
+                We strive to exceed customer expectations at every turn.
+                <br />
+                <br />
+                We are forward thinking in our approach to CG creation, utilizing the latest technology such as Unreal Engine 5 with lumen and nanite support, Houdini and Substance Designer.
+                <br />
+                <br />
+                We believe in achieving higher cost performance and faster iteration by full utilization these technologies.
+                <br />
+                <br />
+                We offer a support support period for our clients even after delivery of service to ensure their all needs are met.
+                </p>
+
+            </article>
             <div className="info-container">
                 <img src={jaakko} alt="Portrait of Jaakko Saari" />
                 <article>
